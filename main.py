337d1958db990cdbf0d67add85cb6efaba3f51ed
Hieu Vuong <tvuong5@dons.usfca.edu>
2023-06-30 16:07:37 -0700

    Program can take one username.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from utilities import *
 import sys
+import uuid
 EXIT_FAILURE = 1
-DEFAULT_FILENAME = 'log.txt'
+DEFAULT_FILENAME = f'{uuid.uuid4().hex[:6]}.txt'
 
 if get_log(DEFAULT_FILENAME) == 1:
     sys.exit(EXIT_FAILURE)
 
-day_map = process_log(DEFAULT_FILENAME)
+if len(sys.argv) == 1:
+    # no user name is given
+    day_map = process_log(DEFAULT_FILENAME)
+elif len(sys.argv) == 2:
+    # one username is given
+    day_map = process_log(DEFAULT_FILENAME, sys.argv[1])
+else:
+    print("Too many arguments provided.")
+    sys.exit(EXIT_FAILURE)
 total_coding_time = get_total_coding_time(day_map=day_map)
 print(format_coding_time(total_coding_time))
-clean_up(DEFAULT_FILENAME)
